7062405118d7eee9a94732f379e8afabd2bc600e
Kazbekgit <kazbekgit@gmail.com>
2022-03-03 22:41:43 +0300

    использовал Array.from()

--- js/main.js
+++ js/main.js
@@ -20,7 +20,8 @@ function isRelevantLength(currentStroke, maxLength) {
 isRelevantLength(' ', 1); // заглушка, чтобы линтер не возмущался
 
 const OBJECTS_NUMBER = 25;
-const COMMENTS_MAX_NUMBER = 1000;
+const COMMENTS_MAX_NUMBER = 10;
+
 const messages = [
   'Всё отлично! В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -28,46 +29,31 @@ const messages = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
+
 const names = ['Луселия Сантус', 'Сузана Виейра', 'Маркус Палмейра', 'Каролина Дикманн', 'Вивиан Пазмантер', 'Мурилу Бенисиу', 'Джованна Антонелли'];
 
-const getUniquePositiveNumber = (maxValue) => {
-  const usedValues = [];
-  return function () {
-    let randomValue = getRandomInt(1, maxValue);
-    if(!usedValues.length) {
-      usedValues.push(randomValue);
-      return randomValue;
-    }
-    while(usedValues.includes(randomValue)) {
-      if(usedValues.length === maxValue) {
-        throw new Error(`Массив из ${maxValue} уникальных чисел заполнен!`);
-      }
-      randomValue = getRandomInt(1, maxValue);
-    }
-    usedValues.push(randomValue);
-    return randomValue;
+const getRandomElement = (arr) => arr[getRandomInt(0, arr.length - 1)];
+
+const createComment = function(index){
+  return {
+    id: index,
+    avatar: `img/avatar-${getRandomInt(1, 6)}.svg`,
+    message: getRandomElement(messages),
+    name: getRandomElement(names),
   };
 };
+const usersComments = Array.from( { length: getRandomInt(0, COMMENTS_MAX_NUMBER) }, (_, index) => createComment(index + 1));
 
-const getUniqueId = getUniquePositiveNumber(OBJECTS_NUMBER);
-const getUniqueURL = getUniquePositiveNumber(OBJECTS_NUMBER);
-const getUniqueCommentId = getUniquePositiveNumber(COMMENTS_MAX_NUMBER);
-
-const createPhotoDescription = function() {
+const createPhotoDescription = function(index) {
   return {
-    id: getUniqueId(),
-    url: `photos/${getUniqueURL()}.jpg`,
+    id: index,
+    url: `photos/${index}.jpg`,
     description: 'Эта фотография заряжена положительной энергией. Если смотреть на нее 59 сек трижды в день, то Ваша жизнь измениться. Но это не точно.',
     likes: getRandomInt(15, 200),
-    comments: {
-      id: getUniqueCommentId(),
-      avatar: `img/avatar-${getRandomInt(1, 6)}.svg`,
-      message: messages[getRandomInt(1, messages.length - 1)],
-      name: names[getRandomInt(1, names.length - 1)],
-    }
+    comments: usersComments,
   };
 };
 
-const photoArray = Array.from({length: OBJECTS_NUMBER}, createPhotoDescription);
+const photos = Array.from({length: OBJECTS_NUMBER}, (_, index) => createPhotoDescription(index + 1));
 
-photoArray.forEach(()=>{}); // заглушка, чтобы линтер не возмущался
+photos.forEach( () => {} ); // заглушка, чтобы линтер не возмущался
